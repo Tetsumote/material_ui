06a21a384bf1f92577898870d5fa6cd21fb70b7d
Paweł Grajewski <unionproject88@gmail.com>
2019-11-28 20:12:24 +0100

    add x overflow protection

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { withStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary
+  }
+});
+
+const App = withStyles(styles)(({ classes }) => (
+  <div className={classes.root}>
+    <Grid container spacing={4}>
+      <Grid item xs={12} sm={6} md={3}>
+        <Paper className={classes.paper}>xs=12 sm=6 md=3</Paper>
+      </Grid>
+      <Grid item xs={12} sm={6} md={3}>
+        <Paper className={classes.paper}>xs=12 sm=6 md=3</Paper>
+      </Grid>
+      <Grid item xs={12} sm={6} md={3}>
+        <Paper className={classes.paper}>xs=12 sm=6 md=3</Paper>
+      </Grid>
+      <Grid item xs={12} sm={6} md={3}>
+        <Paper className={classes.paper}>xs=12 sm=6 md=3</Paper>
+      </Grid>
+    </Grid>
+  </div>
+));
 
 export default App;
